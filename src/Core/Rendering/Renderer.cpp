fee3a021afe3047f635c0cf9b0c3d1898a1d0766
ollie22115 <olivercoates60@googlemail.com>
2025-08-07 11:21:25 +0100

    adding an assertion for if multiple API's (not currently allowed) are set

--- src/Core/Rendering/Renderer.cpp
+++ src/Core/Rendering/Renderer.cpp
@@ -8,7 +8,17 @@
 namespace Strike {
 
 	std::unique_ptr<Renderer> Strike::Renderer::createRenderer() {
+#ifdef STRIKE_DEBUG
+		uint32_t totalRendererAPISet = 0;
+
+#ifdef STRIKE_OPENGL
+		totalRendererAPISet++;
+#endif
+		//include other rendering API's when necessary
+		STRIKE_ASSERT(totalRendererAPISet < 2, LOG_PLATFORM_CORE, "Cannot have more than one renderer API set at once!");
 		
+#endif
+
 		switch (platform) {
 			case RendererPlatform::None:
 				STRIKE_ASSERT(false, LOG_PLATFORM_CORE, "No Renderer API bound!");
