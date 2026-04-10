ef7e1eaf6d9639758ffc44dfe9b2421c1fcd50c1
ollie22115 <olivercoates60@googlemail.com>
2026-04-10 18:31:45 +0100

    added getTranslation() and getScale() to Transform.h

--- src/Core/Scene/Component/Transform.h
+++ src/Core/Scene/Component/Transform.h
@@ -13,6 +13,22 @@ namespace Strike {
 		operator glm::mat4& () { return transformMatrix; }
 		operator const glm::mat4& () const { return transformMatrix; }
 		inline glm::vec4 operator*(const glm::vec4& other) const  { return transformMatrix * other; }
+
+		inline glm::vec3 getTranslation() const {
+			//TODO!!! check this
+			return glm::vec3(transformMatrix[3][0], transformMatrix[3][1], transformMatrix[3][2]);
+		}
+
+		inline glm::vec3 getScale() const {
+			//TODO!!! check this
+			return glm::vec3(transformMatrix[0][0], transformMatrix[1][1], transformMatrix[2][2]);
+		}
+
+		/*
+		inline glm::vec3 getRotation() const {
+			//TODO!!!
+		}
+		*/
 	};
 
 }
